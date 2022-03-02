17aa64dde06191a14ee4c07be9eeecdc3e76389e
Anja Reimer <anja.reimer@itt.uni-stuttgart.de>
2022-03-02 15:28:12 +0100

    Added test for mixtures, fixed wca mixtures

--- src/eos/reference_perturbation_wca.rs
+++ src/eos/reference_perturbation_wca.rs
@@ -38,12 +38,16 @@ impl<D: DualNum<f64>> HelmholtzEnergyDual<D> for ReferencePerturbationWCA {
                     * 0.5; // MIXING RULE not clear!!!
                 let d_ij = (d[i] + d[j]) * 0.5; // (d[i] * p.sigma[i] + d[j] * p.sigma[j]) * 0.5;
                 let q_ij = (q[i] + q[j]) * 0.5;
-                a += x[i] * x[j] * (-eta_a[[i, j]] * 0.5 + 1.0) / (-eta_a[[i, j]] + 1.0).powi(3)
-                    * (-q_ij.powi(3) + (rs_ij * p.sigma_ij[[i, j]]).powi(3))
-                    - ((-eta_b[[i, j]] * 0.5 + 1.0) / (-eta_b[[i, j]] + 1.0).powi(3))
-                        * (-d_ij.powi(3) + (rs_ij * p.sigma_ij[[i, j]]).powi(3))
+
+                a += x[i]
+                    * x[j]
+                    * ((-eta_a[[i, j]] * 0.5 + 1.0) / (-eta_a[[i, j]] + 1.0).powi(3)
+                        * (-q_ij.powi(3) + (rs_ij * p.sigma_ij[[i, j]]).powi(3))
+                        - ((-eta_b[[i, j]] * 0.5 + 1.0) / (-eta_b[[i, j]] + 1.0).powi(3))
+                            * (-d_ij.powi(3) + (rs_ij * p.sigma_ij[[i, j]]).powi(3)))
             }
         }
+
         -a * state.moles.sum().powi(2) * 2.0 / 3.0 / state.volume * PI
     }
 }
@@ -51,12 +55,12 @@ impl<D: DualNum<f64>> HelmholtzEnergyDual<D> for ReferencePerturbationWCA {
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::parameters::utils::test_parameters;
+    use crate::parameters::utils::{test_parameters, test_parameters_mixture};
     use approx::assert_relative_eq;
     use ndarray::arr1;
 
     #[test]
-    fn test_delta_a0_bh() {
+    fn test_delta_a0_wca_pure() {
         let moles = arr1(&[2.0]);
 
         // m = 12.0, t = 4.0, rho = 1.0
@@ -71,6 +75,29 @@ mod test {
         };
         let state = StateHD::new(reduced_temperature, reduced_volume, moles.clone());
         let a = pt.helmholtz_energy(&state) / moles[0];
+        assert_relative_eq!(a, 0.258690311450425, epsilon = 1e-10);
+    }
+    #[test]
+    fn test_delta_a0_wca_mixture() {
+        let moles = arr1(&[1.7, 0.3]);
+        let reduced_temperature = 4.0;
+        let reduced_density = 1.0;
+        let reduced_volume = (moles[0] + moles[1]) / reduced_density;
+
+        let p = test_parameters_mixture(
+            arr1(&[24.0, 24.0]),
+            arr1(&[6.0, 6.0]),
+            arr1(&[1.0, 1.0]),
+            arr1(&[1.0, 1.0]),
+        );
+        let d = diameter_wca(&p, reduced_temperature);
+        let q = diameter_q_wca(&p, reduced_temperature);
+
+        let pt = ReferencePerturbationWCA {
+            parameters: Rc::new(p),
+        };
+        let state = StateHD::new(reduced_temperature, reduced_volume, moles.clone());
+        let a = pt.helmholtz_energy(&state) / (moles[0] + moles[1]);
         assert_relative_eq!(a, 0.258690311450425, epsilon = 1e-10);
     }
 }
